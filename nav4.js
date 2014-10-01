157bfba8ad8c879103764f3822cc5f0ae53b7fc6
tvxqljw <jingwenlee22@gmail.com>
2014-10-01 11:14:16 +0800

    1

---
+++ nav4.js
@@ -0,0 +1,30 @@
+var nav4 =(function(){
+    bindClick = function(els, mask){
+        if(!els || !els.length){return;}
+        var isMobile = "ontouchstart" in window;
+        for(var i=0,ci; ci = els[i]; i++){
+            ci.addEventListener("click", evtFn, false);
+        }
+
+        function evtFn(evt, ci){
+            ci =this;
+            for(var j=0,cj; cj = els[j]; j++){
+                if(cj != ci){
+                    console.log(cj);
+                    cj.classList.remove("on");
+                }
+            }
+            if(ci == mask){mask.classList.remove("on");return;}
+            switch(evt.type){
+                case "click":
+                    var on = ci.classList.toggle("on");
+                    mask.classList[on?"add":"remove"]("on");
+                    break;
+            }
+        }
+        mask.addEventListener(isMobile?"touchstart":"click", evtFn, false);
+    }
+    return {"bindClick":bindClick};
+})();/*  |xGv00|2b60bfccf1a3f1a7735b14e0d7a1d841 *//**
+ * Created by lijingwen on 2014-10-01.
+ */
